12d580172d56c345c5b7707502ca7d32d1169daf
speech <dumpinfo@163.com>
2020-06-13 05:40:20 +0800

    update

--- runcov.sh
+++ runcov.sh
@@ -1,3 +1,3 @@
 lcov --capture --directory ./  --output-file test.info --test-name test
 genhtml test.info --output-directory output --title "code analysis" --show-details --legend
-gprof -b ./TerrainRL | gprof2dot | dot -Tpng -o call.png
+gprof -b ./DeepTerrainRL | gprof2dot | dot -Tsvg -o call.svg
